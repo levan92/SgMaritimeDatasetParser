f6de975bae84ef48c02df320b269ae3a559b7bd6
DigitalHubRocks <dstadh17@gmail.com>
2019-08-21 10:35:40 +0800

    contextualise now knows the set based on the frame name

--- contextualise.py
+++ contextualise.py
@@ -1,8 +1,10 @@
 import os
 
-context = '/media/dh/Data/SGMaritimeDataset/VIS_Onshore/Imgs'
-annot_file = '/media/dh/Data/SGMaritimeDataset/VIS_Onshore_annot.txt'
-contexted_annot_file = '/media/dh/Data/SGMaritimeDataset/VIS_Onshore_contexted_annot.txt'
+context = '/media/dh/DATA4TB/Datasets/SGMaritimeDataset/'
+annot_file = '/media/dh/DATA4TB/Datasets/SGMaritimeDataset/SMD_train.txt'
+# contexted_annot_file = '/media/dh/DATA4TB/Datasets/SGMaritimeDataset/VIS_Onshore_annot.txt'
+contexted_annot_file = annot_file.replace('.txt','_contexted.txt')
+assert not os.path.exists(contexted_annot_file)
 
 assert os.path.exists(annot_file),'Annotation txt file does not exist'
 assert os.path.isdir(context),'Directory given is not a directory'
@@ -14,8 +16,16 @@ new_lines = []
 for line in lines:
     splits = line.split()
     img_name = splits[0]
+
+    if 'NIR' in img_name:
+        set_name = 'NIR'
+    elif 'VIS_OB' in img_name:
+        set_name = 'VIS_Onboard'
+    else:
+        set_name = 'VIS_Onshore'
+
     rest = splits[1:]
-    img_path = os.path.join(context, img_name)
+    img_path = os.path.join(*[context, set_name, 'Imgs', img_name])
     assert os.path.exists(img_path),'Image {} does not exists!'.format(img_path)
     rest.insert(0, img_path)
     newline = ' '.join(rest)
